e2633488e11ed73e2a6537bb662c40f9a140b25d
Jeongnim Kim <jeongnim.kim@intel.com>
2026-05-13 17:31:06 +0000

    Fix put/amo order and add nic8/nic2 tests.

--- pbs_contention.sh
+++ pbs_contention.sh
@@ -35,40 +35,65 @@ export NNODES=${nnodes}
 export BASE=${BASE:-${HOME}/shmem-workspace/build/latest}
 OSU_BUILD=${OSU_BUILD:-${HOME}/shmem-workspace/build/osu-bench}
 
-##declare -ax SOS_LIST=(sos sos-cma)
-#declare -ax SOS_LIST=(sos sos-barrier)
-#
-#export NIC_MAPPER=${NIC_MAPPER:-}
-#echo "INFO: NIC_MAPPER=${NIC_MAPPER}"
-#echo "INFO: SHMEM_OFI_NIC_POLICY=${SHMEM_OFI_NIC_POLICY}"
-#
-min_bytes=512
-max_bytes=32768
+min_bytes=${MIN_BYTES:-1024}
+max_bytes=${MAX_BYTES:-8192}
+app=cust_nail_clone
+PUT_LIST=(0)
+AMO_LIST=(0 20 40 60 80 100)
+#PUT_LIST=(0 20 40 60 80 100)
 
 X=sos
 export CVARS="-genv LD_LIBRARY_PATH=$BASE/install/$X/lib:$LD_LIBRARY_PATH"
-shm_exe=${OSU_BUILD}/openshmem/cust_nail_clone
-for ppn in "${PPN_LIST[@]}"; do
+shm_exe=${OSU_BUILD}/openshmem/${app}
+export NIC_MAPPER=""
+for ppn in 64  $BIND1C_MAX; do
   nranks=$(( nnodes * ppn ))
-  for put in 0 20 40 60 80 100; do
-    for amo in 0 20 40 60 80 100; do
-      echo "SOS: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} "
-      timeout 300 mpirun ${CVARS} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${NIC_MAPPER} ${shm_exe} heap  ${amo} ${put}
-    done
+  for put in "${PUT_LIST[@]}"; do
+    for amo in "${AMO_LIST[@]}"; do
+      echo "SOS: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=8"
+      timeout 300 mpirun ${CVARS} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${NIC_MAPPER} ${shm_exe} heap ${put} ${amo} ${min_bytes} ${max_bytes} \
+        | tee -a ${out_dir}/sos.nic8.N${nnodes}.p${ppn}.dat 
+      done
   done
 done
 
+export NIC_MAPPER=${HOME}/shmem-workspace/cxi_mapper.sh
+for ppn in 64  $BIND1C_MAX; do
+  nranks=$(( nnodes * ppn ))
+  for put in "${PUT_LIST[@]}"; do
+    for amo in "${AMO_LIST[@]}"; do
+      echo "SOS: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=2"
+      timeout 300 mpirun ${CVARS} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${NIC_MAPPER} ${shm_exe} heap ${put} ${amo} ${min_bytes} ${max_bytes} \
+        | tee -a ${out_dir}/sos.nic2.N${nnodes}.p${ppn}.dat 
+      done
+  done
+done
+
+if [[ "${HAVE_SHMEMX}" -eq 1 ]]; then
 source ${HOME}/shmem-workspace/config.cray-shmem.sh
-shm_exe=${OSU_BUILD}/shmemx/cust_nail_clone
-for ppn in "${PPN_LIST[@]}"; do
+shm_exe=${OSU_BUILD}/shmemx/${app}
+for ppn in 64  $BIND1C_MAX; do
   nranks=$(( nnodes * ppn ))
-  # if SHMEM_OFI_NIC_POLICY=USER, set NVAR; otherwise, empty (use default provider selection)
-  NVAR=$([[ "${SHMEM_OFI_NIC_POLICY}" == "USER" ]] && echo "-genv SHMEM_OFI_NIC_MAPPING=0:0-$((ppn/2-1));4:$((ppn/2))-$((ppn-1))")
-  for put in 0 20 40 60 80 100; do
-    for amo in 0 20 40 60 80 100; do
-      echo "SHMEMX: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} "
-      timeout 300 mpirun ${NVAR} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${shm_exe} heap ${shm_exe} heap  ${amo} ${put}
-    done
+  NVAR=""
+  for put in "${PUT_LIST[@]}"; do
+    for amo in "${AMO_LIST[@]}"; do
+      echo "SHMEMX: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=8"
+      timeout 300 mpirun ${NVAR} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${shm_exe} heap ${put} ${amo} ${min_bytes} ${max_bytes} \
+        | tee -a ${out_dir}/shmemx.nic8.N${nnodes}.p${ppn}.dat
+      done
   done
 done
 
+export SHMEM_OFI_NIC_POLICY=USER
+for ppn in 64  $BIND1C_MAX; do
+  nranks=$(( nnodes * ppn ))
+  NVAR="-genv SHMEM_OFI_NIC_MAPPING=0:0-$((ppn/2-1));4:$((ppn/2))-$((ppn-1))"
+  for put in "${PUT_LIST[@]}"; do
+    for amo in "${AMO_LIST[@]}"; do
+      echo "SHMEMX: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=2"
+      timeout 300 mpirun ${NVAR} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${shm_exe} heap ${put} ${amo} ${min_bytes} ${max_bytes} \
+        | tee -a ${out_dir}/shmemx.nic2.N${nnodes}.p${ppn}.dat
+    done
+  done
+done
+fi
