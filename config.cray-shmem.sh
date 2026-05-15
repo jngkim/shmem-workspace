e4b4bec4a60c46a7c5983cc77baa45ceff8d5b7a
Jeongnim Kim <jeongnim.kim@intel.com>
2026-05-15 23:23:23 +0000

    Set LIBRARY_PATH as well.

--- config.cray-shmem.sh
+++ config.cray-shmem.sh
@@ -10,6 +10,7 @@ export SMA_ROOT=/opt/cray/pe/sma/11.7.4/ofi/sma
 export DSMML_ROOT=/opt/cray/pe/dsmml/0.3.1/dsmml
 export PMI_ROOT=/opt/cray/pe/pmi/6.1.15
 export LD_LIBRARY_PATH=${SMA_ROOT}/lib64:${PMI_ROOT}/lib:${DSMML_ROOT}/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=${SMA_ROOT}/lib64:${PMI_ROOT}/lib:${DSMML_ROOT}/lib:$LIBRARY_PATH
 
 export SHMEM_USE_DSMML_SSHEAP=0
 export PALS_PMI=pmi
