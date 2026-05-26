2a8c5e84535d1b1dc39cb37aea52ebd3838b4d19
Jeongnim Kim <jeongnim.kim@gmail.com>
2026-05-26 07:36:26 -0700

    Benchmark random and bucket at the same time.

--- pbs_contention.sh
+++ pbs_contention.sh
@@ -5,7 +5,7 @@
 #PBS -j n
 #PBS -A Intel-Punchlist
 
-set -x
+#set -x
 
 nnodes_avail=$(wc $PBS_NODEFILE| awk '{print $1}')
 nnodes=${NNODES:-$nnodes_avail}
@@ -19,10 +19,13 @@ TIMESTAMP=$(date +%Y%m%d)
 
 source ${HOME}/shmem-workspace/config.sh
 source ${HOME}/shmem-workspace/cpu_bind.sh exclude
+export BASE=${BASE:-${HOME}/shmem-workspace/build/latest}
+OSU_BUILD=${OSU_BUILD:-${HOME}/shmem-workspace/build/osu-bench}
+min_bytes=${MIN_BYTES:-1024}
+max_bytes=${MAX_BYTES:-8192}
 
 out_dir=${HOME}/shmem-workspace/results/${CLUSTER}/${JOB_NAME}.${JOBID}
 mkdir -p ${out_dir}
-
 env | grep CLUSTER >> ${out_dir}/env.txt
 env | grep PLATFORM >> ${out_dir}/env.txt
 env | grep SLURM >> ${out_dir}/env.txt
@@ -30,46 +33,55 @@ env | grep FI | grep -v LMOD  >> ${out_dir}/env.txt
 #cp $PBS_NODEFILE ${out_dir}/hostfile
 cat $PBS_NODEFILE |  cut -d '.' -f 1 | tee -a ${out_dir}/hostfile
 
-export BASE=${BASE:-${HOME}/shmem-workspace/build/latest}
-OSU_BUILD=${OSU_BUILD:-${HOME}/shmem-workspace/build/osu-bench}
-SOS_LIB=${SOS_LIB:-sos}
+declare -ax APP_LIST=(random bucket)
+declare -Ax APP_BIN
+APP_BIN["random"]=cust_nail_random
+APP_BIN["bucket"]=cust_nail_random_bucket
+
+PUT_LIST=(0 1)
+AMO_LIST=(0 1)
+
+SOS=(sos sos-xpmem)
+out_dir_top=$out_dir
 
 # applicaion speicifc settings
-app=cust_nail_clone
-min_bytes=${MIN_BYTES:-1024}
-max_bytes=${MAX_BYTES:-8192}
+for app_tag in "${APP_LIST[@]}"; do
+  out_dir=${out_dir_top}/${app_tag}
+  mkdir -p ${out_dir}
+  app=${APP_BIN[$app_tag]}
 
-# skip PUT scan since it is expacted to be less critical than AMO scan
-PUT_LIST=(0)
-AMO_LIST=(0 20 40 60 80 100)
-
-export CVARS="-genv LD_LIBRARY_PATH=$BASE/install/$SOS_LIB/lib:$LD_LIBRARY_PATH"
-shm_exe=${OSU_BUILD}/openshmem/${app}
-export NIC_MAPPER=""
-for ppn in 64  $BIND1C_MAX; do
-  nranks=$(( nnodes * ppn ))
-  for put in "${PUT_LIST[@]}"; do
-    for amo in "${AMO_LIST[@]}"; do
-      echo "SOS: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=8"
-      opts="heap ${put} ${amo} ${min_bytes} ${max_bytes}"
-      timeout 300 mpirun ${CVARS} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${NIC_MAPPER} ${shm_exe} ${opts} \
-        | tee -a ${out_dir}/sos.nic8.N${nnodes}.p${ppn}.dat 
+  for SOS_LIB in "${SOS[@]}"; do
+    export CVARS="-genv LD_LIBRARY_PATH=$BASE/install/$SOS_LIB/lib:$LD_LIBRARY_PATH"
+    shm_exe=${OSU_BUILD}/openshmem/${app}
+    export NIC_MAPPER=""
+    for ppn in 64  $BIND1C_MAX; do
+      nranks=$(( nnodes * ppn ))
+      for put in "${PUT_LIST[@]}"; do
+        for amo in "${AMO_LIST[@]}"; do
+          echo "SOS: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=8"
+          opts="heap ${put} ${amo} ${min_bytes} ${max_bytes}"
+          timeout 300 mpirun ${CVARS} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${NIC_MAPPER} ${shm_exe} ${opts} \
+            | tee -a ${out_dir}/${SOS_LIB}.nic8.N${nnodes}.p${ppn}.dat 
+          done
+        done
       done
-  done
-done
 
-export NIC_MAPPER=${HOME}/shmem-workspace/cxi_mapper.sh
-for ppn in 64  $BIND1C_MAX; do
-  nranks=$(( nnodes * ppn ))
-  for put in "${PUT_LIST[@]}"; do
-    for amo in "${AMO_LIST[@]}"; do
-      echo "SOS: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=2"
-      opts="heap ${put} ${amo} ${min_bytes} ${max_bytes}"
-      timeout 300 mpirun ${CVARS} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${NIC_MAPPER} ${shm_exe} ${opts} \
-        | tee -a ${out_dir}/sos.nic2.N${nnodes}.p${ppn}.dat 
+      export NIC_MAPPER=${HOME}/shmem-workspace/cxi_mapper.sh
+      for ppn in 64  $BIND1C_MAX; do
+        nranks=$(( nnodes * ppn ))
+        for put in "${PUT_LIST[@]}"; do
+          for amo in "${AMO_LIST[@]}"; do
+            echo "SOS: N=${nnodes} PPN=${ppn} AMO=${amo} PUT=${put} NIC=2"
+            opts="heap ${put} ${amo} ${min_bytes} ${max_bytes}"
+            timeout 300 mpirun ${CVARS} -np ${nranks} -ppn ${ppn} ${BINDINGS[$ppn]} ${NIC_MAPPER} ${shm_exe} ${opts} \
+              | tee -a ${out_dir}/${SOS_LIB}.nic2.N${nnodes}.p${ppn}.dat 
+            done
+          done
+        done
+
       done
-  done
-done
+
+    done  # app_tag
 
 if [[ "${HAVE_SHMEMX}" -eq 1 ]]; then
 source ${HOME}/shmem-workspace/config.cray-shmem.sh
